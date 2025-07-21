dd3e99f20a2641b4b0375f965e55a9de75878b9e
kanatbay12 <kana7292@gmail.com>
2025-07-22 00:42:26 +0500

    ee

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,9 @@
-// СКРИПТ ДЛЯ УПРАВЛЕНИЯ МЕНЮ
-        document.addEventListener('DOMContentLoaded', () => {
-            const menuToggleBtn = document.getElementById('menu-toggle-btn');
-            const sideNav = document.getElementById('side-nav');
+        const firebaseConfig = {
+  apiKey: "AIzaSyBK7irH8u4HNgcnxAusGNzj7WHzJj0ka_M",
+  authDomain: "autodiag-kana.firebaseapp.com",
+  projectId: "autodiag-kana",
+  storageBucket: "autodiag-kana.firebasestorage.app",
+  messagingSenderId: "643930483001",
+  appId: "1:643930483001:web:be4621c52f342abc73a0e8"
 
-            if (menuToggleBtn && sideNav) {
-                // Открытие/закрытие меню по клику на кнопку
-                menuToggleBtn.addEventListener('click', (event) => {
-                    event.stopPropagation(); // Предотвращаем "всплытие" события
-                    menuToggleBtn.classList.toggle('is-active');
-                    sideNav.classList.toggle('is-open');
-                });
-
-                // Закрытие меню по клику вне его области
-                document.addEventListener('click', (event) => {
-                    const isClickInsideNav = sideNav.contains(event.target);
-                    const isClickOnToggleBtn = menuToggleBtn.contains(event.target);
-
-                    if (!isClickInsideNav && !isClickOnToggleBtn && sideNav.classList.contains('is-open')) {
-                        menuToggleBtn.classList.remove('is-active');
-                        sideNav.classList.remove('is-open');
-                    }
-                });
-            }
-        });
+};
