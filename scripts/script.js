4a2dc1f81bcaf18db4e6a7c34938f4dc00d1de5f
kanatbay12 <kana7292@gmail.com>
2025-07-22 15:24:30 +0500

    AdminPANEL

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
-        const firebaseConfig = {
-  apiKey: "AIzaSyBK7irH8u4HNgcnxAusGNzj7WHzJj0ka_M",
-  authDomain: "autodiag-kana.firebaseapp.com",
-  projectId: "autodiag-kana",
-  storageBucket: "autodiag-kana.firebasestorage.app",
-  messagingSenderId: "643930483001",
-  appId: "1:643930483001:web:be4621c52f342abc73a0e8"
+        // ===============================================================
+        // UID АДМИНИСТРАТОРА
+        // Замените "YOUR_ADMIN_UID" на реальный UID вашего аккаунта
+        // администратора из Firebase Authentication
+        // ===============================================================
+        const adminUid = "9B9EWaCLoWOq8J8f8vrmP6Yx4vs1";
 
-};
+        const firebaseConfig = {
+          apiKey: "AIzaSyBK7irH8u4HNgcnxAusGNzj7WHzJj0ka_M",
+          authDomain: "autodiag-kana.firebaseapp.com",
+          projectId: "autodiag-kana",
+          storageBucket: "autodiag-kana.firebasestorage.app",
+          messagingSenderId: "643930483001",
+          appId: "1:643930483001:web:be4621c52f342abc73a0e8"
+        };
